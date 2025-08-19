de8fa1ab04bdd281eaaa5dbc6954d8a9af7feeef
zakrevskyi <zakr1995@gmail.com>
2025-08-19 18:28:59 +0300

    fix

--- alembic/env.py
+++ alembic/env.py
@@ -2,14 +2,13 @@ from logging.config import fileConfig
 
 from sqlalchemy import engine_from_config
 from sqlalchemy import pool
-import os
-import sys
+
 from alembic import context
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
 config = context.config
-sys.path.insert(0, os.path.realpath(os.path.join(os.path.dirname(__file__), '..')))
+
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
 if config.config_file_name is not None:
@@ -17,8 +16,7 @@ if config.config_file_name is not None:
 
 # add your model's MetaData object here
 # for 'autogenerate' support
-from sql_engine import Base
-import models
+from models import Base
 # target_metadata = mymodel.Base.metadata
 target_metadata = Base.metadata
 
